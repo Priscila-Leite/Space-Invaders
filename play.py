39c8d527d142817718d39cfa595387ae4f8c6022
Priscila Leite <priscilalss@id.uff.br>
2023-11-10 15:06:30 -0300

    Add Aliens

--- play.py
+++ play.py
@@ -21,7 +21,30 @@ spaceship.set_position(screen_width/2 - spaceship.width/2, screen_height-spacesh
 # Definição da velocidade da nave e dos tiros
 velocity_ship = 180
 velocity_shot = 180
+fim = False
 
+aliens = []
+alien_sprite = Sprite('Imagens/Play/alien.png')
+linhas, colunas = 3, 5
+distancia = alien_sprite.width + alien_sprite.height
+direcao = 1
+y = 0
+def aliens_create():
+    distancia_y = distancia
+    for linha in range(linhas):
+        list_linha = []
+        distancia_x = distancia
+        for coluna in range(colunas):
+            alien = alien_sprite
+            alien_x = distancia_x
+            alien_y = distancia_y
+            distancia_x += distancia
+            list_linha.append([alien, alien_x, alien_y])
+        distancia_y += distancia
+        aliens.append(list_linha)
+
+
+aliens_create()
 # Inicializa lista de tiros na tela vazia
 shoots = []
 
@@ -34,6 +57,38 @@ def update_screen():
     spaceship.set_position(spaceship.x, spaceship.y)
     spaceship.draw()
 
+    colisao = False
+    perdeu = False
+    velocity_alien_x = 200
+
+    global y
+    global direcao
+    y = 0
+
+    for linha in aliens:
+        if colisao:
+            break
+        for alien in linha:
+            if alien[1] < 0:
+                direcao = 1
+                colisao = True
+                y += 1
+                break
+
+            if alien[1] > screen_width - alien[0].width:
+                direcao = -1
+                colisao = True
+                y += 1
+                break
+
+    for linha in aliens:
+        for alien in linha:
+            alien[2] += y * alien[0].height
+            alien[0].set_position(alien[1], alien[2])
+            alien[1] += direcao * velocity_alien_x * screen.delta_time()
+            alien[0].draw()
+            if alien[2]+alien[0].height > spaceship.y:
+                return True
 
     # Desenha cada tiro da lista movendo para cima
     for shot in shoots:
@@ -52,8 +107,9 @@ def shoot(x):
     shot.set_position(x, spaceship.y-spaceship.height)
     shoots.append(shot)
 
-# 
+
 def Play(modo):
+    global fim
 
     pressed = Window.get_keyboard()
 
@@ -75,14 +131,15 @@ def Play(modo):
         shoot(x)
 
     # Voltar ao menu
-    if pressed.key_pressed('ESC'):
+    if pressed.key_pressed('ESC') or fim:
         spaceship.set_position(screen_width/2 - spaceship.width/2, screen_height-spaceship.height*3)
         while len(shoots) > 1:
             shoots.pop()
         return 'None'
     
     # Atualizar tela
-    update_screen()
+    fim = update_screen()
+    screen.draw_text(str(int(1/screen.delta_time())), 1, 1, 24, bold=True, color=(255, 255, 255))
 
     # Continuar jogando
     return Play
